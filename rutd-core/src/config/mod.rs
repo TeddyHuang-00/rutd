cfe2d05e484d76726007668d4d8c1d099a03f06a
Nan Huang <teddyhuangnan@gmail.com>
2025-05-03 19:11:48 -0400

    refactor: extract package name resolution logic in Config::new function

--- rutd-core/src/config/mod.rs
+++ rutd-core/src/config/mod.rs
@@ -33,8 +33,15 @@ impl Config {
     /// 2. Configuration file
     /// 3. Default values
     pub fn new() -> Result<Self> {
-        let config_file = format!("~/.{}/config.toml", env!("CARGO_PKG_NAME"));
-        let env_var_prefix = env!("CARGO_PKG_NAME").to_uppercase() + "_";
+        // Get the package name from the compiled binary name
+        // This should be resolved to `rutd` for all binaries
+        // (e.g. `rutd`, `rutd-cli`, `rutd-tui`, etc.)
+        let pkg_name = env!("CARGO_PKG_NAME")
+            .split_once('-')
+            .map_or(env!("CARGO_PKG_NAME"), |(name, _)| name)
+            .to_string();
+        let config_file = format!("~/.{}/config.toml", pkg_name);
+        let env_var_prefix = pkg_name.to_uppercase() + "_";
 
         Ok(Figment::new()
             .merge(Serialized::defaults(Config::default()))
