010b6974d846d34a3ad455ad7e6ca639485c46d0
Nan Huang <teddyhuangnan@gmail.com>
2025-05-04 17:24:26 -0400

    refactor: Move completion logic to cli crate for clarity

--- rutd-cli/src/main.rs
+++ rutd-cli/src/main.rs
@@ -1,14 +1,8 @@
 use std::process::ExitCode;
 
-use clap::CommandFactory;
-use clap_complete::CompleteEnv;
-use rutd_cli::{app, cli::Cli};
+use rutd_cli::app;
 
 fn main() -> ExitCode {
-    // Check if we're being called for completion generation
-    // Pass the factory function `cli_factory`
-    CompleteEnv::with_factory(Cli::command).complete();
-
     // Call the main application function
     app()
 }
