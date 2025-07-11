4bf50529b20e3c1aeebfbf21988d84c7e62df5c5
Nan Huang <teddyhuangnan@gmail.com>
2025-07-10 22:19:42 -0500

    style: Fix formatting of documentation comment for validate_utf8_or_empty function

--- rutd-cli/src/completer/utils.rs
+++ rutd-cli/src/completer/utils.rs
@@ -1,7 +1,8 @@
 use std::ffi::OsStr;
 
 /// Validates UTF-8 input for completion functions.
-/// Returns None if the input is not valid UTF-8, which should result in no completions.
+/// Returns None if the input is not valid UTF-8, which should result in no
+/// completions.
 pub fn validate_utf8_or_empty(input: &OsStr) -> Option<&str> {
     input.to_str()
 }
